de8e1ba4f892d9f8c460902b7f99aff97e3906ca
usthbi10058 <tranhieucg@gmail.com>
2022-01-14 01:06:09 +0700

    'update'

--- 1/AwesomeProject/index.js
+++ 1/AwesomeProject/index.js
@@ -2,14 +2,12 @@
  * @format
  */
 
-import React from 'react'
-import{Text} from 'react-native'
-import {AppRegistry} from 'react-native'
-import App from './App'
-import {name as appName} from './app.json'
-import MainScreen from './screens/MainScreen'
-import WellcomeScreen from './screens/WellcomeScreen'
-/*AppRegistry.registerComponent(appName,() => WellcomeScreen);*/
-AppRegistry.registerComponent(appName,() => MainScreen);
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import HomeScreen from './screens/HomeScreen';
+import MainScreen from './screens/MainScreen';
+import SigninScreen from './screens/SigninScreen';
+import SignupScreen from './screens/SignupScreen';
 
-
+AppRegistry.registerComponent(appName, () => HomeScreen);
